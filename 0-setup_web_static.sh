ee3fd6227ce85555b1640b7d9b7a41c2646aa2ba
SrDri <juanjcarabali@gmail.com>
2021-08-27 07:34:54 +0700

    Fix task

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,33 +1,23 @@
 #!/usr/bin/env bash
 # install and configure arbnb static
 
-# nginx
 sudo apt-get update
 sudo apt-get install -y nginx
 
-# data folder
 sudo mkdir -p /data
-# owner
 sudo chown -R ubuntu:ubuntu /data
 
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 
-# fake html
-HTML="<html>
-<head>
-</head>
-<body>
-Holberton School
-</body>
-</html>"
+HTML="Holberton school - test"
 
 sudo echo -e "$HTML" > /data/web_static/releases/test/index.html
 
 rm -rf /data/web_static/current
 ln -s /data/web_static/releases/test/ /data/web_static/current
 
-line="\\location /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n"
+line="location /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n"
 sudo sed -i "43i\ $line" /etc/nginx/sites-available/default
 
 sudo service nginx restart
